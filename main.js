89dc16c4a55811c940510d9da7e502db9c9f402f
zxllyy <zxya456@gmail.com>
2024-01-25 18:16:37 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function imgSlider(anything) {
+  document.querySelector(`.starBucks`).src = anything;
+}
+function changeCircleColor(color) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+}
